6c2b8c6a787eb3fe50c575f3a9ef7cc9604c3bcc
Adrien Lachaize <adrien.lachaize@gmail.com>
2022-05-19 14:54:07 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>TheNest <span class="text-indigo-500">front</span></h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
